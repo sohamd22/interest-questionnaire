7a27c7c4c529805fc54c956ace219a2e4a90ed7f
Soham Daga <soham.daga22@gmail.com>
2024-08-28 15:50:32 -0700

    Increased admin sizes

--- client/src/pages/Admin.jsx
+++ client/src/pages/Admin.jsx
@@ -7,59 +7,59 @@ const Admin = ({ submitters }) => {
         <div className="flex flex-col items-center gap-8">
             <img src={logo} alt="devlabs-logo" />
             <div className="flex flex-wrap gap-8">
-                <div className="flex flex-col gap-2">
+                <div className="flex flex-col gap-5">
                     <div className="flex flex-col gap-1 items-center">
-                        <p className={`text-2xl font-semibold`}>Web Development</p>
+                        <p className={`text-4xl font-bold`}>Web Dev.</p>
                         <hr className="w-full" />
                     </div>
                     <ul className="flex flex-col gap-1">
                         {
-                            submitters.filter(submitter => submitter.domain==="Web Development").map(submitter => <li className="text-base" key={submitter._id}>- {submitter.name}</li>)
+                            submitters.filter(submitter => submitter.domain==="Web Development").map(submitter => <li className="text-3xl font-semibold" key={submitter._id}>- {submitter.name}</li>)
                         }
                     </ul>
                 </div>
-                <div className="flex flex-col gap-2">
+                <div className="flex flex-col gap-5">
                     <div className="flex flex-col gap-1 items-center">
-                        <p className={`text-2xl font-semibold`}>App Development</p>
+                        <p className={`text-4xl font-bold`}>App Dev.</p>
                         <hr className="w-full" />
                     </div>
                     <ul className="flex flex-col gap-1">
                         {
-                            submitters.filter(submitter => submitter.domain==="App Development").map(submitter => <li className="text-base" key={submitter._id}>- {submitter.name}</li>)
+                            submitters.filter(submitter => submitter.domain==="App Development").map(submitter => <li className="text-3xl font-semibold" key={submitter._id}>- {submitter.name}</li>)
                         }
                     </ul>
                 </div>
-                <div className="flex flex-col gap-2">
+                <div className="flex flex-col gap-5">
                     <div className="flex flex-col gap-1 items-center">
-                        <p className={`text-2xl font-semibold`}>AI/ML</p>
+                        <p className={`text-4xl font-bold`}>AI/ML</p>
                         <hr className="w-full" />
                     </div>
                     <ul className="flex flex-col gap-1">
                         {
-                            submitters.filter(submitter => submitter.domain==="AI/ML").map(submitter => <li className="text-base" key={submitter._id}>- {submitter.name}</li>)
+                            submitters.filter(submitter => submitter.domain==="AI/ML").map(submitter => <li className="text-3xl font-semibold" key={submitter._id}>- {submitter.name}</li>)
                         }
                     </ul>
                 </div>
-                <div className="flex flex-col gap-2">
+                <div className="flex flex-col gap-5">
                     <div className="flex flex-col gap-1 items-center">
-                        <p className={`text-2xl font-semibold`}>Cybersecurity</p>
+                        <p className={`text-4xl font-bold`}>Cybersecurity</p>
                         <hr className="w-full" />
                     </div>
                     <ul className="flex flex-col gap-1">
                         {
-                            submitters.filter(submitter => submitter.domain==="Cybersecurity").map(submitter => <li className="text-base" key={submitter._id}>- {submitter.name}</li>)
+                            submitters.filter(submitter => submitter.domain==="Cybersecurity").map(submitter => <li className="text-3xl font-semibold" key={submitter._id}>- {submitter.name}</li>)
                         }
                     </ul>
                 </div>
-                <div className="flex flex-col gap-2">
+                <div className="flex flex-col gap-5">
                     <div className="flex flex-col gap-1 items-center">
-                        <p className={`text-2xl font-semibold`}>UI/UX Design</p>
+                        <p className={`text-4xl font-bold`}>UI/UX</p>
                         <hr className="w-full" />
                     </div>
                     
                     <ul className="flex flex-col gap-1">
                         {
-                            submitters.filter(submitter => submitter.domain==="UI/UX Design").map(submitter => <li className="text-base" key={submitter._id}>- {submitter.name}</li>)
+                            submitters.filter(submitter => submitter.domain==="UI/UX Design").map(submitter => <li className="text-3xl font-semibold" key={submitter._id}>- {submitter.name}</li>)
                         }
                     </ul>
                 </div>
